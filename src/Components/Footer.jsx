c1ebe63c826943ad8cb9c2bef935b808abb74cec
mukh1ddinov_o1 <halilullohmuhiddinov5557788@gmail.com>
2024-10-09 16:34:30 +0500

    Shopping Site yaratildi

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-3 bg-secondary d-flex justify-content-center'>
+        <p className='fw-bolder text-white'>&copy; Code With Mukh1ddinov_o1</p>
+    </div>
+  )
+}
+
+export default Footer
